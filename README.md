73de2286c72b350ec2f03fd7ef0232795c6961c7
Kadir Pekel <kadirpekel@gmail.com>
2010-09-22 09:53:15 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Meryl Extras
+============
+
+This project provides extra plugins and extensions to the [Meryl](http://coffeemate.github.com/meryl) project.
+
---
+++ index.js
@@ -0,0 +1,21 @@
+/*!
+ * Meryl
+ * Copyright(c) 2010 Kadir Pekel.
+ * MIT Licensed
+ */
+
+/*
+ * Modules dependencies
+ */
+ var meryl = require('meryl');
+
+/*
+ * Function that probes and returns add-on for given name
+ * @param {String} name
+ */
+module.exports = function(name) {
+  // TODO: find an add-on and return it.
+  console.log('searching for extra: ' + name);
+  throw 'not implemented yet';
+}
+
