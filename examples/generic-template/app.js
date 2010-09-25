fb2baf5dd63cdb9740a48b42c2d101d1ada594b1
Kadir Pekel <kadirpekel@gmail.com>
2010-09-26 00:24:03 +0300

    refactored templating examples and updated README adding 'eco' templating example

--- examples/generic-template/app.js
+++ examples/generic-template/app.js
@@ -1,15 +1,17 @@
 var meryl = require('meryl'),
   merylex = require('./../../index');
-  
-var gtemplate = merylex('generictemplate');
 
-// Use generic template with all default options
-// It uses John Ressig's micro template library by default.
-meryl.x('render', gtemplate()); 
+var generictemplate = merylex('generictemplate');
 
-meryl.h('GET /', function() {
-  this.render('home', {people: ['bob', 'alice', 'jane', 'meryl']});
-});
-
-require('http').createServer(meryl.cgi()).listen(3000);
+require('http').createServer(
+	meryl
+	// Use generic template with all default options
+  // It uses John Ressig's micro template library by default.
+  .x('render', generictemplate())
+  .h('GET /', function() {
+	    this.render('home', {people: ['bob', 'alice', 'jane', 'meryl']});
+	  }
+  )
+  .cgi()
+).listen(3000);
 console.log('serving http://localhost:3000');
