689deeab800d9e0cf2505d47acab5f918bf45d28
Kadir Pekel <kadirpekel@gmail.com>
2010-09-29 11:52:12 -0400

    added paperboy plugin and fixed staticfile plugin

---
+++ examples/paperboy/app.js
@@ -0,0 +1,15 @@
+var meryl = require('meryl'),
+  merylex = require('./../../index');
+
+var paperboy = merylex('paperboy');
+
+meryl.p('GET *', paperboy());
+
+meryl.h('GET /', function () {
+  this.status = 301;
+  this.headers['Location'] = '/static/index.html';
+  this.send();
+});
+
+require('http').createServer(meryl.cgi()).listen(3000);
+console.log('serving http://localhost:3000');
